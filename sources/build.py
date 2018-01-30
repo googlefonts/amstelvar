c5a64b5727d1e4d38fc4a8a49dd970e8d0b32c8c
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2018-01-30 11:27:56 -0500

    Add Italic designspace and build script

--- sources/build.py
+++ sources/build.py
@@ -1,14 +1,16 @@
 from fontmake.font_project import FontProject
 from fontTools.varLib import build
 
-ufos = [
+# Roman
+
+romans = [
 	"Roman/Amstelvar.ufo",
 	"Roman/Amstelvar-opszmax.ufo",
 ]
 
 project = FontProject()
 project.run_from_ufos(
-	ufos, 
+	romans, 
 	output=("ttf-interpolatable"), # FIXME this also build master_ttf and should not.
 	remove_overlaps=False, 
 	reverse_direction=False, 
@@ -16,7 +18,30 @@ project.run_from_ufos(
 
 designSpace = "Amstelvar-Roman.designspace"
 outfile = "../fonts/Amstelvar-VF.ttf"
-finder = lambda s: s.replace("Roman", "master_ttf_interpolatable").replace(".ufo", ".ttf")
+finder = lambda s: s.replace("Roman/", "master_ttf_interpolatable/").replace(".ufo", ".ttf")
+varfont, _, _ = build(designSpace, finder)
+print "Saving Variable Font..."
+varfont.save(outfile)
+print "DONE!"
+
+# Italic
+
+italics = [
+	"Italic/Amstelvar-Italic.ufo",
+	"Italic/Amstelvar-Italic-opszmax.ufo",
+]
+
+project = FontProject()
+project.run_from_ufos(
+	italics, 
+	output=("ttf-interpolatable"), # FIXME this also build master_ttf and should not.
+	remove_overlaps=False, 
+	reverse_direction=False, 
+	use_production_names=False)
+
+designSpace = "Amstelvar-Italic.designspace"
+outfile = "../fonts/Amstelvar-Italic-VF.ttf"
+finder = lambda s: s.replace("Italic/", "master_ttf_interpolatable/").replace(".ufo", ".ttf")
 varfont, _, _ = build(designSpace, finder)
 print "Saving Variable Font..."
 varfont.save(outfile)
