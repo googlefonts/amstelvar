666c4d661f8506ec52cca1470ea4234c07101f4a
CJ Dunn <cjdunn@Lappy-447.local>
2019-06-28 12:22:44 -0400

    cleanup build script

--- build-amstelvar.sh
+++ build-amstelvar.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 # open terminal, type: sh build.sh
-# this version requires newer fonttools to use --output-dir command
 
 
 printf "Build fonts…\n"
 
-# cd source_roboto || exit
-
-
-#if ! python "sources/Amstelvar-NewCharset/build-amstelvar.py"
 
 if ! python "build-amstelvar.py"
 
@@ -18,13 +13,6 @@ if ! python "build-amstelvar.py"
         printf "Unable to run pyhon script.  Build canceled." 1>&2
         exit 1
 fi
-
-
-# if ! fontmake -m "source/designspace/test.designspace" -o variable --no-production-names --output-dir './fonts/Test_vf'
-#     then
-#         printf "Unable to build var font.  Build canceled." 1>&2
-#         exit 1
-# fi
 
 
 
